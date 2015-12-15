09bca8a6bfa629d5ed33f526301a77396db44602
jk <jk>
2015-12-15 10:38:52 +0100

    implemented DiceInterface

--- Dice.java
+++ Dice.java
@@ -1,11 +1,11 @@
 import java.util.Random;
 /**
  * This is class Dice, it creates a Dice object that will return a int between low and high
- * 
- * @author Jaakko Koistinen 
+ *
+ * @author Jaakko Koistinen
  * @version (a version number or a date)
  */
-public class Dice
+public class Dice implements DiceInterface
 {
     // instance variables
     private int low;
@@ -24,11 +24,11 @@ public class Dice
 
     /**
      * rollDice method
-     * 
+     *
      * @param  no parameters
      * @return     int result
      */
-    
+
     public int rollDice() {
         Random random = new Random();
 		int result = random.nextInt(high) + low;
---
+++ DiceInterface.java
@@ -0,0 +1,3 @@
+public interface DiceInterface {
+  public int rollDice();
+}
